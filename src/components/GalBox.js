3d387433d9996e5fc247e19eb06ab27d0d6310e8
dongjin-ina <djshin319@gmail.com>
2025-03-28 22:50:57 +0900

    gallery anime

--- src/components/GalBox.js
+++ src/components/GalBox.js
@@ -1,5 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RevealOnce from "./RevealOnce";
+import photoBbeum1 from "../icons/photo_bbeum_1.png";
+import photoBbeum2 from "../icons/photo_bbeum_2.png";
+import photoDung1 from "../icons/photo_dung_1.png";
+import photoDung2 from "../icons/photo_dung_2.png";
+import photoDung3 from "../icons/photo_dung_3.png";
 
 const importAll = (r) => r.keys().map(r);
 const galImgs = importAll(require.context("../images", false, /gal\d+\.jpg$/));
@@ -8,7 +13,14 @@ const galThumbImgs = importAll(
 );
 
 const GalBox = () => {
+  const [isFlashing, setIsFlashing] = useState(true);
+  const [dungPos, setDungPos] = useState(0);
+  const [dungDir, setDungDir] = useState(-1);
+  const [dungRot, setDungRot] = useState(0);
+  const [dung, setDung] = useState(1);
+
   const [imgNum, setImgNum] = useState(-1);
+
   const setNum = (i) => () => setImgNum(i);
   const delNum = () => setImgNum(-1);
   const nextNum = () => {
@@ -18,9 +30,55 @@ const GalBox = () => {
     imgNum === 0 ? setImgNum(galImgs.length - 1) : setImgNum((i) => i - 1);
   };
 
+  useEffect(() => {
+    if (isFlashing) {
+      const flashInterval = setInterval(() => {
+        setDung((prev) => (prev === 1 ? 2 : 1));
+      }, 300);
+      setTimeout(() => {
+        clearInterval(flashInterval);
+        setIsFlashing(false);
+      }, 2400);
+      return () => clearInterval(flashInterval);
+    } else {
+      setDung(3);
+      setDungDir((d) => -d);
+      let cPos = dungPos;
+      const moveInterval = setInterval(() => {
+        cPos = cPos + dungDir;
+        if (cPos === -4 || cPos === 0) {
+          clearInterval(moveInterval);
+          setIsFlashing(true);
+          setDung(1);
+        }
+        setDungPos(cPos);
+      }, 300);
+      return () => clearInterval(moveInterval);
+    }
+  }, [isFlashing]);
+
   return (
     <RevealOnce className="gal-box">
-      <div className="title">아이콘 필요</div>
+      <div>
+        {dung}, {dungPos}
+      </div>
+      <div className="photo-title">
+        <img
+          className="photo-dung"
+          src={dung === 1 ? photoDung1 : dung === 2 ? photoDung2 : photoDung3}
+          alt=""
+          style={{
+            transform: ` translate(-3rem, 2rem) translate(${
+              0.5 * dungPos
+            }rem) scale(${dung === 3 ? dungDir : 1},1)`,
+          }}
+        />
+        <img
+          className="photo-bbeum"
+          src={dung === 3 ? photoBbeum1 : photoBbeum2}
+          alt=""
+        />
+      </div>
       <div className="gal-grid">
         {galThumbImgs.map((src, index) => (
           <img key={`gal-${index}`} src={src} alt="" onClick={setNum(index)} />
